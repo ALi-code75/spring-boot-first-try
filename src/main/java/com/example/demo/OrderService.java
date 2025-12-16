cd0701efda2c9c2ae8d704fe043c4f6109c8dee6
alibu <alibu@Z590>
2025-12-16 23:43:00 +0300

    Final for mosh free course

--- src/main/java/com/example/demo/OrderService.java
+++ src/main/java/com/example/demo/OrderService.java
@@ -1,9 +1,19 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
 	
 	private PaymentService paymentService;
 	
+	
+	public OrderService(PaymentService paymentService , int x) {
+		
+}
+	@Autowired
 	public OrderService(PaymentService paymentService) {
 			this.paymentService = paymentService;
 	}
@@ -12,5 +22,11 @@ public class OrderService {
 		
 		paymentService.proccessPayment(10);
 	}
+
+//	public void setPaymentService(PaymentService paymentService) {
+//		this.paymentService = paymentService;
+//	}
+	
+	
 
 }
--- src/main/java/com/example/demo/SpringBootFirstTryApplication.java
+++ src/main/java/com/example/demo/SpringBootFirstTryApplication.java
@@ -2,13 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootFirstTryApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(SpringBootFirstTryApplication.class, args);
-		var orderService = new OrderService(new PayPalPaymentService());
+	ConfigurableApplicationContext context = 	SpringApplication.run(SpringBootFirstTryApplication.class, args);
+	var orderService = 	context.getBean(OrderService.class);
+	
+	
+		//var orderService = new OrderService(new PayPalPaymentService());
+		//var orderService = new OrderService();
+		//orderService.setPaymentService(new PayPalPaymentService());
 		orderService.placeOrder();
 	}
 
